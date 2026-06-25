19ecabba4c3f89ec9b2cfaa749069cae8a41ab8f
Tanmay katara <154496645+tanmayx999@users.noreply.github.com>
2026-06-25 21:59:23 +0530

    Time: 282 ms (94.62%), Space: 0B (100%) - LeetHub

--- 3793-find-users-with-high-token-usage/3793-find-users-with-high-token-usage.sql
+++ 3793-find-users-with-high-token-usage/3793-find-users-with-high-token-usage.sql
@@ -1,9 +1,5 @@
-SELECT user_id,
-COUNT(prompt) AS prompt_count,
-ROUND(AVG(tokens), 2) AS avg_tokens
+SELECT user_id, COUNT(prompt) AS prompt_count, ROUND(AVG(tokens), 2) AS avg_tokens
 FROM prompts
 GROUP BY user_id
-HAVING COUNT(*) >= 3 AND MAX(tokens) > AVG(tokens)
-ORDER BY 
-avg_tokens DESC,
-user_id ASC
+HAVING COUNT(prompt) >= 3 AND MAX(tokens) > AVG(tokens)
+ORDER BY avg_tokens DESC, user_id ASC
