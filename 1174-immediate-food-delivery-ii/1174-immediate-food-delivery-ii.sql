acd860a78f3ccc52000b4efaea78f4b9817e4e7e
Tanmay katara <154496645+tanmayx999@users.noreply.github.com>
2026-07-03 21:29:13 +0530

    Time: 779 ms (17.63%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,7 +1,11 @@
-SELECT ROUND(
-           AVG(IF(order_date = customer_pref_delivery_date, 100, 0)),2) AS immediate_percentage FROM Delivery
+SELECT 
+    ROUND(
+        AVG(IF(order_date = customer_pref_delivery_date, 100, 0)), 
+        2
+    ) AS immediate_percentage
+FROM Delivery
 WHERE (customer_id, order_date) IN (
     SELECT customer_id, MIN(order_date)
     FROM Delivery
-    GROUP BY customer_id  
+    GROUP BY customer_id
 );
