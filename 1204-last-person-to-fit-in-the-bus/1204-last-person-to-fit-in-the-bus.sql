99410a83401355a0b8ed9e3b541464151db181f7
Tanmay katara <154496645+tanmayx999@users.noreply.github.com>
2026-07-06 17:15:14 +0530

    Time: 832 ms (60.12%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,9 +1,9 @@
 SELECT person_name
 FROM (
     SELECT person_name,
-                    SUM(weight) OVER (ORDER BY turn) AS cumulative_weight
+    SUM(weight) OVER (ORDER BY turn) AS cumulative_weight
     FROM Queue
-) AS running_totals
+) AS runing_total
 WHERE cumulative_weight <= 1000
 ORDER BY cumulative_weight DESC 
 LIMIT 1;
