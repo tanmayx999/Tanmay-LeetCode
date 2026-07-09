8a46b18ff43a98c719475bb5fd7e9e1da65d7b76
Tanmay katara <154496645+tanmayx999@users.noreply.github.com>
2026-07-09 13:16:00 +0530

    Time: 1427 ms (39.85%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,20 +1,20 @@
 (
-    -- Part 1: Most Active User
-    SELECT u.name AS results
-    FROM MovieRating mr
-    JOIN Users u ON mr.user_id = u.user_id
-    GROUP BY mr.user_id, u.name
-    ORDER BY COUNT(mr.movie_id) DESC, u.name ASC
-    LIMIT 1
+SELECT u.name AS results
+FROM Users u
+JOIN MovieRating mr 
+ON u.user_id = mr.user_id
+GROUP BY mr.user_id, u.name
+ORDER BY COUNT(mr.user_id) DESC, u.name ASC 
+LIMIT 1
 )
 UNION ALL
 (
-    -- Part 2: Highest Rated Movie in Feb 2020
-    SELECT m.title AS results
-    FROM MovieRating mr
-    JOIN Movies m ON mr.movie_id = m.movie_id
-    WHERE mr.created_at >= '2020-02-01' AND mr.created_at <= '2020-02-29'
-    GROUP BY mr.movie_id, m.title
-    ORDER BY AVG(mr.rating) DESC, m.title ASC
-    LIMIT 1
+SELECT m.title AS results
+FROM Movies m
+JOIN MovieRating mr
+ON m.movie_id = mr.movie_id
+WHERE mr.created_at >= '2020-02-01' AND mr.created_at <= '2020-02-29'
+GROUP BY mr.movie_id, m.movie_id
+ORDER BY AVG(mr.rating) DESC, m.title ASC
+LIMIT 1
 );
