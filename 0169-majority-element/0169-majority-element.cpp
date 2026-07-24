3e0f2bbe3ecfe10e2521a6a49934d5307d44cc48
Tanmay katara <154496645+tanmayx999@users.noreply.github.com>
2026-07-24 23:37:44 +0530

    Time: 0 ms (100%), Space: 28.2 MB (70.33%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+class Solution {
+public:
+    int majorityElement(std::vector<int>& nums) {
+        int candidate = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+};
