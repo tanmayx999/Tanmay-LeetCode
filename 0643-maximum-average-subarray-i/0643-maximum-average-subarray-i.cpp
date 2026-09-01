02a2f7b87330d7ae668d5b4d55e44b2f8c9860fc
Tanmay katara <154496645+tanmayx999@users.noreply.github.com>
2026-09-01 17:30:57 +0530

    Time: 2 ms (40.46%), Space: 113.7 MB (61.4%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -1,11 +1,9 @@
 #include <vector>
 #include <numeric>
 #include <algorithm>
-
 class Solution {
 public:
     double findMaxAverage(std::vector<int>& nums, int k) {
-        // Calculate the sum of the first window of size k
         double current_sum = 0;
         for (int i = 0; i < k; ++i) {
             current_sum += nums[i];
@@ -13,13 +11,10 @@ public:
         
         double max_sum = current_sum;
         
-        // Slide the window across the array
         for (int i = k; i < nums.size(); ++i) {
             current_sum += nums[i] - nums[i - k];
             max_sum = std::max(max_sum, current_sum);
         }
-        
-        // Return the maximum average
         return max_sum / k;
     }
 };
