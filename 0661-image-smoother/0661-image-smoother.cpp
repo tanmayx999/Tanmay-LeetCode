7d3681ed53c9bf6fae34fe34f2e410fed06afae4
Tanmay katara <154496645+tanmayx999@users.noreply.github.com>
2026-09-02 21:07:12 +0530

    Time: 2 ms (82.15%), Space: 26 MB (94.15%) - LeetHub

---
+++ 0661-image-smoother/0661-image-smoother.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
+        int m = img.size();
+        int n = img[0].size();
+        vector<vector<int>> res(m, vector<int>(n, 0));
+
+        for (int r = 0; r < m; ++r) {
+            for (int c = 0; c < n; ++c) {
+                int sum = 0;
+                int count = 0;
+
+                // Check 3x3 neighborhood
+                for (int dr = -1; dr <= 1; ++dr) {
+                    for (int dc = -1; dc <= 1; ++dc) {
+                        int nr = r + dr;
+                        int nc = c + dc;
+
+                        // Bounds check
+                        if (nr >= 0 && nr < m && nc >= 0 && nc < n) {
+                            sum += img[nr][nc];
+                            count++;
+                        }
+                    }
+                }
+                res[r][c] = sum / count; // Integer division handles floor rounding
+            }
+        }
+
+        return res;
+    }
+};
