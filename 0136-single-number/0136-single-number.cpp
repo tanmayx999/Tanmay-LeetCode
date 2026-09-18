658a5b653d26cb96759b01d809e84957e1adf991
Tanmay katara <154496645+tanmayx999@users.noreply.github.com>
2026-09-18 17:26:52 +0530

    Time: 886 ms (5%), Space: 20.7 MB (58.52%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,14 +3,13 @@ public:
     int singleNumber(vector<int>& nums) {
         int n = nums.size();
         for(int i=0; i<n; i++){
-            int num = nums[i];
             int count = 0;
             for(int j=0; j<n; j++){
-                if(nums[j] == num){
+                if(nums[j] == nums[i]){
                     count++;
                 }
             }
-            if(count == 1) return num;
+            if(count == 1) return nums[i];
         }
         return -1;
     }
